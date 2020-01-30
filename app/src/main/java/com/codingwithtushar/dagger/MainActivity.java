e94b50294ec100499d01c4f7863c51418da1a102
Tushar Garg <tushargarg2201@gmail.com>
2020-01-30 08:46:09 +0530

    initial commit

---
+++ app/src/main/java/com/codingwithtushar/dagger/MainActivity.java
@@ -0,0 +1,14 @@
+package com.codingwithtushar.dagger;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
